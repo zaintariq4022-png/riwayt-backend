462c06e86de2a0f12c1efe754a0890aa1e2b5450
Asimali112 <asimfyter6@gmail.com>
2026-03-20 23:06:42 +0500

    Add Category model

---
+++ src/models/Category.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+
+const categorySchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    description: {
+      type: String,
+      trim: true,
+    },
+    image: {
+      type: String,
+    },
+    isActive: {
+      type: Boolean,
+      default: true,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model('Category', categorySchema);
