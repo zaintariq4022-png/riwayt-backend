04429b6f9d1a90fd54c83ac744c687b33a9d783e
Asimali112 <asimfyter6@gmail.com>
2026-03-29 20:06:49 +0500

    feat: short description below order button

--- src/models/Product.js
+++ src/models/Product.js
@@ -15,6 +15,7 @@ const productSchema = new mongoose.Schema(
     },
     subcategory: { type: String, default: '' },
     fabric: { type: String, default: '' },
+    shortDesc: { type: String, default: '' },
     sizes: [{ type: String }],
     sizeQtys: { type: Map, of: Number, default: {} },
     colors: [{ type: String }],
